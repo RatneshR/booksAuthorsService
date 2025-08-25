a9a07ae7d2cf5bb214b56873a39a0fcad4740d9c
Ratnesh Rai <ratnesh.rai26@gmail.com>
2025-08-25 23:49:13 +0530

    First Commit

---
+++ src/main/java/com/booksauthors/booksauthorsapi/BooksauthorsapiApplication.java
@@ -0,0 +1,13 @@
+package com.booksauthors.booksauthorsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksauthorsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksauthorsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/booksauthors/booksauthorsapi/BooksauthorsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booksauthors.booksauthorsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksauthorsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
